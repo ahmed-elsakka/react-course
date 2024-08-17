446854f83b3b18c92f98132a9b8ebbabe620e30a
ahmed-elsakka <ahmed.elsakka@outlook.com>
2024-08-17 04:26:29 +0200

    Add custom hook

---
+++ custom-hook/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import useFetch from './useFetch';
+
+function App() {
+  const data = useFetch("https://jsonplaceholder.typicode.com/todos");
+
+  return (
+    <>
+      {data &&
+        data.map((item) => {
+          return <p key={item.id}>{item.title}</p>;
+        })}
+    </>
+  );
+}
+
+export default App;
